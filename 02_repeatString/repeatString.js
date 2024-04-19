8e03f631641459952adcbbfbdff45fd73a9f3719
Harsh Yadav <raoharsh138@gmail.com>
2024-04-19 07:07:43 +0530

    Modify code to pass all the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+// const text = 'hey';
 
+const repeatString = function (text, num) {
+  let new_text = '';
+  let error = "ERROR";
+  if (num < 0) {
+    return error;
+  }
+  for (let i = 0; i < num; i++) {
+
+    new_text = new_text + text;
+  }
+  return new_text;
 };
 
 // Do not edit below this line
